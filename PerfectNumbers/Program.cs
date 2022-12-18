6d169cf5955e7467737120f834df646d583a2843
abdohosny1 <make.y90@yahoo.com>
2022-12-18 23:53:09 +0200

    Add project files.

---
+++ PerfectNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  sum;
+          
+
+
+
+            Console.WriteLine("Input  number  one");
+           int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input  number  Two");
+          int  numTwo = Convert.ToInt32(Console.ReadLine());
+            for (int n = numOne; n <= numTwo; n++)
+            {
+               int i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0} ", n);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag;
+
+            Console.WriteLine("Enter NUmber one ");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Number two ");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPrime numbers between " +
+                              "{0} and {1} are: ", num1, num2);
+
+
+            for (int i = num1; i <= num2; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+
+                flag = true;
+
+                for (int j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag == true)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
